f8ea40dbf590a362b8796f493eed6dce7ecf0919
Maksim Manveiler <maximmanveiler@gmail.com>
2024-10-21 23:40:52 +0500

    last version before lesson

--- src/main/java/io/proj3ct/ReturnBot1/DepartInfoBD.java
+++ src/main/java/io/proj3ct/ReturnBot1/DepartInfoBD.java
@@ -11,6 +11,7 @@ import java.sql.SQLException;
  */
 public class DepartInfoBD {
     private DatabaseConnection databaseConnection = new DatabaseConnection();
+
     /**
      * Извлекает информацию из базы данных по заданному идентификатору.
      * @param data Идентификатор для поиска информации.
